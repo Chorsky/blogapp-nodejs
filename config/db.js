cd2f1daa911d3997a0775d9f9338e3b860c12176
Chorsky <40074744+Chorsky@users.noreply.github.com>
2021-06-25 09:25:49 -0300

    Update db.js

--- config/db.js
+++ config/db.js
@@ -1,7 +1,7 @@
 if(process.env.NODE_ENV == "production"){
 
-    module.exports = {mongoURI:"mongodb+srv://admin:<senha>@blogapp-primeiro-node.0hvsi.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"};
+    module.exports = {mongoURI:"mongodb+srv://<user>:<senha>@blogapp-primeiro-node.0hvsi.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"};
 
 }else{
-    module.exports = {mongoURI:"mongodb+srv://admin:<senha>@blogapp-primeiro-node.0hvsi.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"};
+    module.exports = {mongoURI:"mongodb+srv://<user>:<senha>@blogapp-primeiro-node.0hvsi.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"};
 }
